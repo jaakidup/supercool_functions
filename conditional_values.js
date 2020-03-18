c45f80d4c225c5795f99e66c8a65371a22ca630a
jaakidup <jaakidup@gmail.com>
2020-03-18 20:14:22 +0200

    from laptop

--- conditional_values.js
+++ conditional_values.js
@@ -1,4 +1,11 @@
+
+// user.prefered_timezone is not set, the default value will be set to "America/New_York"
+
 const timezone = user.preffered_timezone || "America/New_York";
+
+
+
+
 
 
 
---
+++ stringers.js
@@ -0,0 +1,9 @@
+let price = 34.234523452345234;
+
+console.log('price :', price.toFixed(2));
+
+
+let num = "8";
+console.log(num.padStart(5, "0"));
+
+
